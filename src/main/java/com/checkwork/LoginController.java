db9ad1ea261baecb706a0f79c9bbf69c7ffe1cbb
souviksaha <souviksaha1215@gmail.com>
2017-06-18 16:28:27 -0700

    first commit

---
+++ src/main/java/com/checkwork/LoginController.java
@@ -0,0 +1,37 @@
+package com.checkwork;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+	public ModelAndView checkUser(HttpServletRequest req, HttpServletResponse res)
+	{
+
+		ModelAndView mv=new ModelAndView();
+
+		String uname=req.getParameter("uname");
+		String pass=req.getParameter("pass");
+
+		if(uname.equals("abc") && pass.equals("123"))
+		{
+			System.out.println("Inside success");
+			mv.setViewName("success.jsp");				
+		}//end of if
+		else
+		{
+			System.out.println("Inside else");
+			mv.setViewName("index.jsp");
+		}
+
+		return mv;
+
+	}//end of ModelAndView
+
+}//end of class LoginController
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+	<form action="login">
+		<label>UserName</label><input type="text" name="uname"><br>
+		<label>PassWord</label><input type="password" name="pass"><br>
+		<input type="submit">
+	</form>
+
+</body>
+</html>
